66f806ccc29f335bc0e0c7491f488389c5b28591
胡垚 <qsct9501@163.com>
2023-06-21 17:02:58 +0800

    fix: change dcgm config

--- start.sh
+++ start.sh
@@ -1,4 +1,22 @@
 #! /bin/sh
+
+# # remove old driver
+# sudo apt clean
+# sudo apt update
+# sudo apt purge nvidia-* -y
+# yes | sudo apt autoremove
+
+
+# # install new driver
+# # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+# # sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# # wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
+# # sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
+# # sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+# # sudo apt-get update
+# # sudo apt-get -y install cuda
+
+# install monitor
 sudo cp node_exporter /usr/bin/
 sudo cp node_exporter.service /etc/systemd/system/
 sudo systemctl daemon-reload
